7835a33c36efdc25144a475497e742edc7a39582
dilip210197 <89332489+dilip210197@users.noreply.github.com>
2026-04-18 20:03:06 +0530

    New

---
+++ Testing_App/Testing_App/Testing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_AppApp.swift
+//  Testing_App
+//
+//  Created by Dilip Shehu on 18/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Testing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Testing_App/Testing_AppTests/Testing_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_AppTests.swift
+//  Testing_AppTests
+//
+//  Created by Dilip Shehu on 18/04/26.
+//
+
+import Testing
+@testable import Testing_App
+
+struct Testing_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
